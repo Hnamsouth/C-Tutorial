dff4e961d5aecc42984f97cde4e04da3fd2f187c
hoang nam <namhvth2204008@fpt.edu.vn>
2023-01-31 10:45:56 +0700

    1

--- Assignment/ASM6/MenuFunction.cs
+++ Assignment/ASM6/MenuFunction.cs
@@ -6,7 +6,62 @@ using System.Threading.Tasks;
 
 namespace CSharp.Assignment.ASM6
 {
-    internal class MenuFunction
+    class MenuFunction
     {
+        public List<Student> students;
+
+        public MenuFunction()
+        {
+            students = new List<Student>();
+        }
+//  1. Thêm sinh viên.
+        public void AddSTD(int id, string ten, int tuoi, string gioitinh, double dToan, double dVatLy, double dHoa)
+        {
+     
+            students.Add(new Student( id,ten,tuoi,gioitinh,dToan,dVatLy,dHoa));
+        }
+//2. Cập nhật thông tin sinh viên bởi ID.
+        public void UpdateSTD(int id, string ten, int tuoi, string gioitinh, double dToan, double dVatLy, double dHoa)
+        {
+                Student s = students.Find(st => st.ID.Equals(id));
+                students.Remove(s);
+                students.Add(new Student(id, ten, tuoi, gioitinh, dToan, dVatLy, dHoa));
+            
+        }
+        //3. Xóa sinh viên bởi ID.
+        public void DeleteSTD(int id)
+        {
+            Student s = students.Find(st => st.ID.Equals(id));
+            students.Remove(s);
+        }
+        //4. Tìm kiếm sinh viên theo tên.
+        public Student SearchSTD(string name)
+        {
+            return students.Find(st => st.Ten.Equals(name));
+        }
+        //5. Sắp xếp sinh viên theo điểm trung bình (GPA).
+        public void SortByabs()
+        {
+            students.Sort((o1, o2) =>
+            {
+                return o1.DiemTB.CompareTo(o2.DiemTB);
+            });
+        }
+        //6. Sắp xếp sinh viên theo tên.
+        public void SortByName()
+        {
+            students.Sort((o1, o2) =>
+            {
+                return o1.Ten.CompareTo(o2.Ten);
+            });
+        }
+        //6. Sắp xếp sinh viên theo ID.
+        public void SortByID()
+        {
+            students.Sort((o1, o2) =>
+            {
+                return o1.ID.CompareTo(o2.ID);
+            });
+        }
     }
 }
--- Assignment/ASM6/Student.cs
+++ Assignment/ASM6/Student.cs
@@ -6,7 +6,42 @@ using System.Threading.Tasks;
 
 namespace CSharp.Assignment.ASM6
 {
-    internal class Student
+    class Student
     {
+        /*
+         *  id, tên, giới tính, tuổi, điểm toán, điểm lý, điểm hóa, điểm trung bình và học lực.
+         */
+        public int ID { get; set; }
+        public string Ten { get; set; }
+        public int Tuoi { get; set; }
+        public string GioiTinh { get; set; } 
+        public double DiemToan { get; set; }
+        public double DiemVatLy { get; set; }
+        public double DiemHoa { get; set; }
+        public double DiemTB { get; set; }
+        public string HocLuc { get; set; }
+
+        public Student() { }
+
+        public Student(int id,string ten, int tuoi,string gioitinh, double dToan, double dVatLy, double dHoa) {
+            this.ID=id;
+            this.Ten=ten;
+            this.Tuoi=tuoi;
+            this.GioiTinh=gioitinh;
+            this.DiemToan=dToan;
+            this.DiemVatLy=dVatLy;
+            this.DiemHoa=dHoa;
+            this.DiemTB = (DiemToan + DiemVatLy + DiemHoa) / 3;
+            TinhHocLuc(this.DiemTB);
+
+        }
+        public void TinhHocLuc(double dtb)
+        {
+            if (dtb >= 8) { this.HocLuc = "Gioi"; }else
+            if(dtb >= 6.5) { this.HocLuc = "Kha"; }else
+            if (dtb >= 5) { this.HocLuc = "Trung Binh"; }
+            else { this.HocLuc = "Yeu"; }
+        }
+
     }
 }
